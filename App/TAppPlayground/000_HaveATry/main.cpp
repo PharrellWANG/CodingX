e54a5fc41f719e1f85abd61264b00e1873a16c69
Pharrell.zx <wzxnuaa@gmail.com>
2018-01-26 12:05:31 +0800

    the playground for have a try has been added

---
+++ App/TAppPlayground/000_HaveATry/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Pharrell_WANG on 26/1/2018.
+//
+
+#include <iostream>
+
+int main(){
+    int myArrayX[3] = {0, 1, 2};
+    std::cout << sizeof(myArrayX) << '\n';
+    std::cout << sizeof myArrayX << '\n';
+    std::cout << myArrayX << '\n';
+}
